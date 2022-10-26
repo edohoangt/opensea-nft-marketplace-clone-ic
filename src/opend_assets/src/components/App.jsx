facc886eea1d75472fc8a79f6643e6adbe758446
Edo <descience.thh10@gmail.com>
2022-10-26 09:33:57 +0700

    initial commit

---
+++ src/opend_assets/src/components/App.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import Header from "./Header";
+import Footer from "./Footer";
+import "bootstrap/dist/css/bootstrap.min.css";
+import homeImage from "../../assets/home-img.png";
+import Item from "./Item";
+
+function App() {
+  const nftId = "rrkah-fqaaa-aaaaa-aaaaq-cai";
+
+  return (
+    <div className="App">
+      <Header />
+      {/* <img className="bottom-space" src={homeImage} /> */}
+      <Item id={nftId} />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
